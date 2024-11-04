a9effa7e59d375928778b4cf2e9b700fca62f0ff
veriajiesaputra <133874605+veriajiesaputra@users.noreply.github.com>
2024-11-04 11:25:58 +0700

    Add files via upload

---
+++ Latihan_18_Kelipatan.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan5;
+
+/**
+ *
+ * @author ASUS
+ */
+
+  public class  Latihan_18_Kelipatan {
+    public static void main(String[] args) {
+        System.out.println("Kelipatan 3.5 dari 3.5 sampai 35:");
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println(i);
+        }
+    }
+}
+  
+
